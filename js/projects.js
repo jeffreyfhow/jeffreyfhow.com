35c61a6b45d18b5d10379488c6d889701c405941
Jeffrey How <jeffrey.f.how@gmail.com>
2017-05-27 21:30:00 -0700

    Add to Git

---
+++ js/projects.js
@@ -0,0 +1,27 @@
+$(function(){
+	// Filter Collapsibles
+	$('input.form-check-input').on('change', function(e){
+		var myId = this.id.replace('catRadios','');
+		if(this.checked) {
+			$('div.card.cat' + myId).removeClass('display-none').addClass('display-flex');
+		} else {
+			$('div.card.cat' + myId).removeClass('display-flex').addClass('display-none');
+		}
+	})
+
+	// Stop Youtube video when closing modal
+	jQuery("div#pzoModal").on("hide.bs.modal", function(e) {
+		var url = $('#pzoFrame').attr('src');
+		$('#pzoFrame').attr('src', '');
+		$('#pzoFrame').attr('src', url);
+	})
+
+	// Scroll to collapsible shown
+ 	$('.collapse').on('shown.bs.collapse', function (e) {
+		var offset = $(e.target).offset();
+		$('html, body').animate({ scrollTop: offset.top-120 });
+	})
+
+ 	// Default collapsible when entering page
+	$(location.hash || '#collapsePZO').collapse('show');
+})
