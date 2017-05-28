c7c03cf7dedaf9ea50f8b64395c3c309c211969b
Jeffrey How <jeffrey.f.how@gmail.com>
2017-05-28 13:19:00 -0700

    Remove console logs

--- js/values.js
+++ js/values.js
@@ -1,11 +1,7 @@
 $(function(){
  	// Default collapsible when entering page
  	if (location.hash) {
- 		console.log("My hash" + location.hash);
- 		console.log($(location.hash));
  		var offset = $(location.hash).offset();
- 		console.log(offset);
  		$('html, body').animate({ scrollTop: offset.top-90 });
  	}
-	console.log("Hello?");
 })
